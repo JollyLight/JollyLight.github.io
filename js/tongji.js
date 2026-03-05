3f04fee30d14994a08c4082286340fcaef215bdf
JollyLight <1279357033@qq.com>
2026-03-05 18:16:46 +0800

    Site updated: 2026-03-05 18:16:44

---
+++ js/tongji.js
@@ -0,0 +1,8 @@
+// 百度统计代码
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?90caf8aa2ab1b8a1b8dd3bb2a2f640cd";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
