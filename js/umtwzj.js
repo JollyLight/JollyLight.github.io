50462d2818ee973e8d52148d909768cb2646742c
JollyLight <1279357033@qq.com>
2026-03-25 21:06:24 +0800

    Site updated: 2026-03-25 21:06:23

--- js/umtwzj.js
+++ js/umtwzj.js
@@ -1,13 +1,3 @@
-// 百度统计代码
-var _hmt = _hmt || [];
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?90caf8aa2ab1b8a1b8dd3bb2a2f640cd";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
-
-
 // umami统计代码
 (function () {
     const scriptElement = document.createElement("script");
