b254b02b740faf391bf520aec08310a26e8e699a
JollyLight <1279357033@qq.com>
2026-03-25 20:52:57 +0800

    Site updated: 2026-03-25 20:52:56

--- js/tongji.js
+++ js/tongji.js
@@ -6,3 +6,12 @@ var _hmt = _hmt || [];
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
+
+
+// umami统计代码
+(function () {
+    const scriptElement = document.createElement("script");
+    scriptElement.src = "https://umami-vercel.neutrino7.top/script.js";
+    scriptElement.setAttribute("data-website-id", "da09c657-ad52-4c9b-a3b9-01564b34a47b");
+    document.head.appendChild(scriptElement);
+})();
